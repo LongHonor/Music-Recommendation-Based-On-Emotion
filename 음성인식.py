6092220d5ce1f9963100f096744776f40edd5188
Hong-HyuckKi <102242005+Hong-HyuckKi@users.noreply.github.com>
2022-05-27 22:50:06 +0900

    Add files via upload
    기존의 음성인식 코드에서 scaler를 생성하는 코드 부분을 삭제한 코드

--- 음성인식.py
+++ 음성인식.py
@@ -120,23 +120,7 @@ path = r'C:\Users\arz61\Documents\오픈소스'
 model = torch.load(os.path.join(path,'model_300.pth'))
 
 
-# In[6]:
-
-
-path = 'C:/Users/arz61/OneDrive/바탕 화면/4학년 1학기 홍혁기/오픈소스SW개론/melon chart/음성인식을통한 감정분류'
-file_list = os.listdir(path)
-file_list_py = [file for file in file_list if file.endswith('.csv')] ## 파일명 끝이 .csv인 경우
-
-
 # In[7]:
-
-
-pd_sample = pd.read_csv(path + '/' +file_list_py[0])
-pd_test = pd.read_csv(path + '/' +file_list_py[1])
-pd_train = pd.read_csv(path + '/' +file_list_py[2])
-
-
-# In[8]:
 
 
 import librosa
@@ -162,128 +146,13 @@ def load_audiofiles(file_name, sample_rate=48000):
 # In[9]:
 
 
-from tqdm import tqdm
-def load_data(data_info, isTrain=True):
-    
-    PATH = "C:/Users/arz61/OneDrive/바탕 화면/4학년 1학기 홍혁기/오픈소스SW개론/melon chart/음성인식을통한 감정분류"
-    if isTrain:
-        train_data = []#음성 feature들을 담는 dictionary
-        train_label = []#학습에 사용할 label을 담는 list
-        
-        file_list = data_info['file_name']
-        emotion_list = data_info['emotion']
-        for file_name, emotion in tqdm(zip(file_list, emotion_list)):
-            
-            hi=join(PATH, 'train_data',file_name)
-            train_data.append(load_audiofiles(hi))
-            train_label.append(emotion)
-            
-        return np.array(train_data), np.array(train_label)
-    
-    else:
-        test_data = []
-        file_list = data_info['file_name']
-    
-        for file_name in tqdm(file_list):
-
-            hi=join(PATH, 'test_data',file_name)
-            test_data.append(load_audiofiles(hi))
-            
-        return np.array(test_data)
-
-#DataFlair - Split the dataset
-train_data, train_label = load_data(pd_train)
-test_data = load_data(pd_test, isTrain=False)
-
-
-# In[10]:
-
-
-def Calculate_Melspectrogram(audio, sample_rate):
-    mel_spec = librosa.feature.melspectrogram(y=audio,
-                                              sr=sample_rate,
-                                              n_fft=1024,
-                                              win_length = 512,
-                                              window='hamming',
-                                              hop_length = 256,
-                                              n_mels=128,
-                                              fmax=sample_rate/2
-                                             )
-    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
-    return mel_spec_db
-
-mel_train = []
-print("Calculate mel spectrograms for train set")
-train_data = np.stack(np.array(train_data),0)
-test_data = np.stack(np.array(test_data),0)
-for i in range(train_data.shape[0]):
-    mel_spectrogram = Calculate_Melspectrogram(train_data[i,:], sample_rate=48000)
-    mel_train.append(mel_spectrogram)
-    print("\r Processed {}/{} files".format(i+1,train_data.shape[0]),end='')
-    
-print('')
-mel_train = np.stack(mel_train,axis=0)
-
-mel_test = []
-for i in range(test_data.shape[0]):
-    mel_spectrogram = Calculate_Melspectrogram(test_data[i,:], sample_rate=48000)
-    mel_test.append(mel_spectrogram)
-    print("\r Processed {}/{} files".format(i+1,test_data.shape[0]),end='')
-    
-print('')
-mel_test = np.stack(mel_test,axis=0)
-
-print(f'mel_train:{mel_train.shape}, mel_test:{mel_test.shape}')
-
-
-# In[11]:
-
-
+import joblib
 from sklearn.preprocessing import StandardScaler
-
-x_train = np.expand_dims(mel_train, 1) #DataNum, 1ch, H, W
-x_test = np.expand_dims(mel_test, 1)
-
-scaler = StandardScaler()
-
-b,c,h,w = x_train.shape
-x_train = np.reshape(x_train, newshape=(b,-1))
-x_train = scaler.fit_transform(x_train)
-x_train = np.reshape(x_train, newshape=(b,c,h,w))
-
-b,c,h,w = x_test.shape
-x_test = np.reshape(x_test, newshape=(b,-1))
-x_test = scaler.transform(x_test)
-x_test = np.reshape(x_test, newshape=(b,c,h,w))
+file_name = 'scaler.pkl'
+scaler = joblib.load(file_name)
 
 
-# In[12]:
-
-
-model.eval()
-
-predicts = []
-
-with torch.no_grad():
-    for data in tqdm(x_test):
-        ###test data를 하나씩 불러와서 학습된 모델로 추론 후 predicts array에 저장합니다.
-        t = torch.Tensor(np.expand_dims(data, 0)).to(device)
-        H = model(t)
-        predict = torch.argmax(H, 1).cpu().detach().numpy()[0]
-        predicts.append(predict)
-        
-print(f'predict_len:{len(predicts)}')
-
-
-# In[13]:
-
-
-###저장된 예측값을 위에서 사용한 label encoder를 이용해 다시 문자열로 역변환합니다.
-predicts1 = le.inverse_transform(predicts)
-predicts1
-
-
-# In[144]:
+# In[42]:
 
 
 path = 'C:/Users/arz61/OneDrive/바탕 화면/4학년 1학기 홍혁기/오픈소스SW개론/melon chart/음성인식을통한 감정분류'
@@ -292,13 +161,13 @@ hi=join(path, 'test5.wav')
 sound.append(load_audiofiles(hi))
 
 
-# In[145]:
+# In[43]:
 
 
 sound
 
 
-# In[146]:
+# In[44]:
 
 
 def Calculate_Melspectrogram(audio, sample_rate):
@@ -322,16 +191,14 @@ mel_sound = np.stack(mel_sound,axis=0)
 print(f'mel_sound:{mel_sound.shape}')
 
 
-# In[147]:
+# In[45]:
 
 
 mel_sound.shape
 
 
-# In[148]:
+# In[46]:
 
-
-from sklearn.preprocessing import StandardScaler
 
 x_sound = np.expand_dims(mel_sound, 1)
 
@@ -341,7 +208,7 @@ x_sound = scaler.transform(x_sound)
 x_sound = np.reshape(x_sound, newshape=(b,c,h,w))
 
 
-# In[149]:
+# In[47]:
 
 
 predicts1 = []
@@ -352,26 +219,22 @@ predict1 = torch.argmax(H, 1).cpu().detach().numpy()[0]
 predicts1.append(predict1)
 
 
-# In[150]:
+# In[48]:
 
 
 predicts1
 
 
-# In[151]:
+# In[49]:
 
 
 answer = le.inverse_transform(predicts1)
 answer[0]
 
 
-# In[22]:
+# In[ ]:
 
-
-get_ipython().system('pip install beautifulsoup4')
-
-
-# In[173]:
+# In[32]:
 
 
 import urllib.parse
@@ -457,13 +320,13 @@ for i in lst50:
 print("완료!")
 
 
-# In[174]:
+# In[ ]:
 
 
 melon = pd.DataFrame(melonList)
 
 
-# In[175]:
+# In[ ]:
 
 
 melonList1 = []
@@ -515,7 +378,7 @@ for i in range (100):
             
 
 
-# In[177]:
+# In[ ]:
 
 
 with open('classified_melonList.csv', 'w', encoding='utf-8-sig', newline='') as f:
